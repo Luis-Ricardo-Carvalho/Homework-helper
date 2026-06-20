d265591ee778d144f59ecb5ed261b2e359af3043
lucasa17 <lucassilva04.rosa@gmail.com>
2026-06-20 17:14:26 -0300

    fix: problem on index page not showing subjects

--- app/src/main/java/br/edu/ifsulminas/mch/homeworkhelper/IndexActivity.java
+++ app/src/main/java/br/edu/ifsulminas/mch/homeworkhelper/IndexActivity.java
@@ -21,8 +21,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.List;
 
 import br.edu.ifsulminas.mch.homeworkhelper.model.Subject;
-import br.edu.ifsulminas.mch.homeworkhelper.model.Task;
-import br.edu.ifsulminas.mch.homeworkhelper.model.persistence.TaskDAO;
+import br.edu.ifsulminas.mch.homeworkhelper.model.persistence.SubjectDAO;
 
 public class IndexActivity extends AppCompatActivity {
 
@@ -39,10 +38,8 @@ public class IndexActivity extends AppCompatActivity {
             return insets;
         });
 
-        // Setup dos componente
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(v -> {
-
             Intent formIntent = new Intent(IndexActivity.this, FormSubjectActivity.class);
             startActivity(formIntent);
         });
@@ -50,54 +47,42 @@ public class IndexActivity extends AppCompatActivity {
         subject_list = findViewById(R.id.subject_list);
         registerForContextMenu(subject_list);
 
-        subject_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Subject subject = (Subject) subject_list.getItemAtPosition(position);
+        subject_list.setOnItemClickListener((parent, view, position, id) -> {
+            Subject subject = (Subject) subject_list.getItemAtPosition(position);
 
-                Intent formActIntent = new Intent(IndexActivity.this, FormSubjectActivity.class);
-
-                // mudar par
-                formActIntent.putExtra(FormSubjectActivity.SUBJECT_KEY, subject);
-
-                startActivity(formActIntent);
-            }
+            Intent formActIntent = new Intent(IndexActivity.this, FormSubjectActivity.class);
+            formActIntent.putExtra(FormSubjectActivity.SUBJECT_KEY, subject);
+            startActivity(formActIntent);
         });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        updateTasksList();
+        updateSubjectList();
     }
 
-    private void updateTasksList(){
-        TaskDAO dao = new TaskDAO(this);
-        List<Task> tasks = dao.listAll();
+    private void updateSubjectList() {
+        SubjectDAO dao = new SubjectDAO(this);
+        List<Subject> subjects = dao.listAll();
 
-        ArrayAdapter<Task> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, tasks);
+        ArrayAdapter<Subject> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, subjects);
         subject_list.setAdapter(adapter);
     }
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        //super.onCreateContextMenu(menu, v, menuInfo);
-        MenuItem itemDelete = menu.add("Concluir Tarefa");
+        MenuItem itemDelete = menu.add("Excluir Disciplina");
 
-        itemDelete.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(@NonNull MenuItem item) {
+        itemDelete.setOnMenuItemClickListener(item -> {
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            Subject subject = (Subject) subject_list.getItemAtPosition(info.position);
 
-                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-                Task task = (Task) subject_list.getItemAtPosition(info.position);
-
-                TaskDAO dao = new TaskDAO(IndexActivity.this);
-                dao.delete(task);
-                updateTasksList();
-                return true;
-            }
+            SubjectDAO dao = new SubjectDAO(IndexActivity.this);
+            dao.delete(subject);
+            updateSubjectList();
+            return true;
         });
     }
 }
